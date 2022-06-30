642fd0d27767cff2105790bc82207f392d3f3221
Giedre <giedre.klima@gmail.com>
2022-06-30 15:42:02 +0300

    add visibility on info, refactor

--- script.js
+++ script.js
@@ -18,21 +18,9 @@ fetch("movies.json")
       let movieItem = document.createElement("div");
       movieItem.classList.add("movie-item");
 
-      let movieTitle = document.createElement("p");
-      movieTitle.textContent = movie.title;
-      movieTitle.classList.add("movie-title");
-
       let imgEl = document.createElement("img");
       imgEl.src = movie.img;
       imgEl.classList.add("movie-img");
-
-      let movieGenresList = document.createElement("ul");
-      movieGenresList.classList.add("movies-genres");
-      movie.genres.map((genre) => {
-        let movieGenre = document.createElement("li");
-        movieGenre.textContent = genre;
-        movieGenresList.append(movieGenre);
-      });
 
       let movieRanking = document.createElement("p");
       movieRanking.textContent = movie.review.ranking;
@@ -45,30 +33,56 @@ fetch("movies.json")
 
       let movieInfoLineEl = document.createElement("div");
       movieInfoLineEl.classList.add("movie-info-line");
-      movieInfoLineEl.append(movieGenresList, movieRanking, movieViews);
+      movieInfoLineEl.append(movieRanking, movieViews);
+
+      let movieTitle = document.createElement("p");
+      movieTitle.textContent = movie.title;
+      movieTitle.classList.add("movie-title");
+
+      let visibleInfo = document.createElement("div");
+      visibleInfo.classList.add("more-info", "hide");
+
+      let movieGenresList = document.createElement("ul");
+      movieGenresList.classList.add("movies-genres");
+      movie.genres.map((genre) => {
+        let movieGenre = document.createElement("li");
+        movieGenre.textContent = genre;
+        movieGenresList.append(movieGenre);
+      });
 
       let movieSummary = document.createElement("p");
       movieSummary.textContent = movie.summary;
+      movieSummary.classList.add("summary");
 
       let movieDirector = document.createElement("p");
-      let isPlural = movie.director.length && "s";
+      let isPlural = movie.director.length > 1 ? "s" : "";
       let directors = movie.director.join(", ");
       movieDirector.innerHTML = `<strong>Director${isPlural}:</strong> ${directors}`;
 
       let movieCast = document.createElement("p");
       let actors = movie.cast.join(", ");
       movieCast.innerHTML = `<strong>Stars:</strong> ${actors}`;
+      movieCast.classList.add("cast");
 
       moviesList.append(movieItem);
 
-      movieItem.append(imgEl);
-      movieItem.append(movieInfoLineEl);
-
+      movieItem.append(imgEl, movieInfoLineEl);
       movieItem.append(movieTitle);
-
-      movieItem.append(movieSummary);
-      movieItem.append(movieDirector);
-      movieItem.append(movieCast);
+      visibleInfo.append(
+        movieGenresList,
+        movieSummary,
+        movieDirector,
+        movieCast
+      );
+      movieItem.addEventListener("mouseenter", () => {
+        movieItem.append(visibleInfo);
+        visibleInfo.classList.add("show");
+        visibleInfo.classList.remove("hide");
+      });
+      movieItem.addEventListener("mouseleave", () => {
+        visibleInfo.classList.remove("show");
+        visibleInfo.classList.add("hide");
+      });
     });
   });
 
